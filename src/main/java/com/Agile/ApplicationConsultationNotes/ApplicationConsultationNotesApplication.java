ff392ebd8dcc16a047ef860b079ad33dd1f8c91d
Anass Serroukh <anasssrk1@gmail.com>
2024-04-15 10:06:46 +0100

    repository initialization

---
+++ src/main/java/com/Agile/ApplicationConsultationNotes/ApplicationConsultationNotesApplication.java
@@ -0,0 +1,13 @@
+package com.Agile.ApplicationConsultationNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationConsultationNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationConsultationNotesApplication.class, args);
+	}
+
+}
